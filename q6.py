329d2b121cd32bd9b06f917aaca9e4eec9577c78
Gulla Naga Poojitha <gullanagapoojitha123@gmail.com>
2026-02-16 15:13:35 +0530

    Update q6.py

--- q6.py
+++ q6.py
@@ -1,2 +1,8 @@
-Write Your Function For the Problem Statement Here
-Team name : Codepy
+# Team Name: Codepy
+
+def convert_temperature(value: float, unit: str) -> float | str:
+    if unit == 'C':
+        return round((value * 9/5) + 32, 1)
+    if unit == 'F':
+        return round((value - 32) * 5/9, 1)
+    return "Invalid Unit"
