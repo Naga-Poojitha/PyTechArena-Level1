a2589f5247bcfb9f7badb9d3b934349adc94beb0
Gulla Naga Poojitha <gullanagapoojitha123@gmail.com>
2026-02-16 14:36:01 +0530

    Update q4.py

--- q4.py
+++ q4.py
@@ -1,2 +1,11 @@
-Write Your Function For the Problem Statement Here
-Team name : Codepy
+# Team name : Codepy
+# Team Name: Codepy
+
+def get_ticket_price(age: int, is_student: bool) -> int:
+    if age < 12:
+        return 8
+    if age >= 65:
+        return 10
+    if is_student:
+        return 12
+    return 15
