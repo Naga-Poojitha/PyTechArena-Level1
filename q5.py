469481d4b5da93bae30a436f8fc9fb6389104ef3
Gulla Naga Poojitha <gullanagapoojitha123@gmail.com>
2026-02-16 15:06:56 +0530

    Update q5.py

--- q5.py
+++ q5.py
@@ -1,2 +1,34 @@
-Write Your Function For the Problem Statement Here
-Team name : Codepy
+# Team name : Codepy
+# Team Name: Codepy
+
+def calculate(expression: str) -> float:
+    expression = expression.replace(" ", "")
+    
+    stack = []
+    num = 0
+    sign = '+'
+    i = 0
+
+    while i < len(expression):
+        char = expression[i]
+
+        if char.isdigit():
+            num = num * 10 + int(char)
+
+        if char in "+-*/" or i == len(expression) - 1:
+            if sign == '+':
+                stack.append(num)
+            elif sign == '-':
+                stack.append(-num)
+            elif sign == '*':
+                stack.append(stack.pop() * num)
+            elif sign == '/':
+                stack.append(stack.pop() / num)
+
+            sign = char
+            num = 0
+
+        i += 1
+
+    result = sum(stack)
+    return round(result, 2)
